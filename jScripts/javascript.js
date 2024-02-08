e413db95abf7e0c4a586f8dec5c3094376fde443
noamb1 <103825684+noamb1@users.noreply.github.com>
2024-02-08 10:25:45 +0200

    Add files via upload

---
+++ jScripts/javascript.js
@@ -0,0 +1,286 @@
+const jsonData = {
+    "generators": [
+
+        {
+
+            "id": 1,
+
+            "pic": "moneyarea.jpeg",
+
+            "title": "הקדמה ותקציר על תרבות הצריכה.",
+
+            "abstract": "תרבות הצריכה מאופיינת בחומריות, חומרנות ורכישת מוצרים מעבר לסיפוק הכרחי דוגמת מזון או בית מגורים.",
+
+            "fullContent": "הרדיפה הזו מדגימה את הצורך האנושי להיות מזוהה עם מוצר או שירות אותו בוחר האדם לצרוך.",
+
+            "cat":"מידע"
+
+        },
+
+        {
+
+            "id": 2,
+
+            "pic": "wastemoney.jpeg",
+
+            "title": "הוצאות מיותרות",
+
+            "abstract": "כסף שנשפך לחינם מבלי שאנו משתמשים בו באמת.",
+
+            "fullContent": "לא מעט אנשים משלמים דמי מנוי לשירותים שהם לא באמת משתמשים בהם, אבל שוכחים או מתעצלים לבטל את החיוב עליהם.",
+
+            "cat":"דרכים לקיזוז הוצאות"
+
+        },
+
+ {
+
+            "id": 3,
+
+            "pic": "flight.jpeg",
+
+            "title": "חוויות משותפות יחד",
+
+            "abstract": "החיסכון נותן לכם את האפשרות להגיע לחופש כלכלי.",
+
+            "fullContent": "חופש כלכלי פירושו החופש לקנות מה שאתם רוצים, לצאת לחופשה שאתם רוצים ואת האפשרות להתמודד בצורה הטובה ביותר עם אירועים בלתי צפויים בחייכם.",
+
+            "cat":"טיפים"
+
+        },
+
+ {
+
+            "id": 4,
+
+            "pic": "super.jpg",
+
+            "title": "ניהול קניות אוכל בצורה חכמה",
+
+            "abstract": "הרכישות בסופר הן חלק מרכזי ובלתי נפרד מההוצאות החודשיות של משפחה.",
+
+            "fullContent": "אותן קניות שאנחנו עושים בסופר מסתכמות באלפי שקלים בחודש ועשרות אלפי שקלים בשנה ולא בכדי ההוצאה עבור קניות ממוקמת כיום בישראל במקום השלישי והמכובד בהוצאות השוטפות שלנו מדי חודש (אחרי דיור ורכב), כשברקע עליית המחירים של השנים האחרונות ויוקר המחיה הכללי בישראל.",
+
+            "cat":"דרכים לקיזוז הוצאות"
+
+        },
+
+ {
+
+            "id": 5,
+
+            "pic": "sea.jpeg",
+
+            "title": "חוויות ללא עלות. ",
+
+            "abstract": "למרות שהחלטנו להתייעל בהוצאות ולחסוך יותר זה לא צריך לבוא על חשבון החוויות שלנו.",
+
+            "fullContent": "ישנן חוויות רבות שאפשר לעשות בחינם: הליכה בחוף הים, סיבוב בפארק ועוד.",
+
+            "cat":"טיפים"
+
+        },
+
+ {
+
+            "id": 6,
+
+            "pic": "talkaboutmoney.jpg",
+
+            "title": "חיבור הילדים לתהליך וחינוך פיננסי לילדים. ",
+
+            "abstract": "איך מדברים עם הילדים על כסף? משום מה יש סוג של “טאבו” על שיח פיננסי בבתים, קל וחומר שיח פיננסי מול הילדים.",
+
+            "fullContent": "זה עלול להרגיש לא בנוח ואולי “טמא” העיסוק בכסף, אבל בעיני זה נושא שצריך להיות “על השולחן” ולא מודחק לאיזו פינה חשוכה.",
+
+            "cat":"מידע"
+
+        },
+
+ {
+
+            "id": 7,
+
+           "pic": "water.jpg",
+
+            "title": "חיסכון במשאבים כמו מים וחשמל.  ",
+
+            "abstract": "הערכות מדברות על כך שעקב בזבוז של משאבי מים ואנרגיה (חשמל וגז ביתי) משפחה ישראלית ממוצעת מורידה לטמיון אלפי שקלים בכל שנה.",
+
+            "fullContent": "על פי רוב מדובר בהצטברות של הרבה מאוד גורמים קטנים שבלי שאנחנו שמים לב ‘זוללים’ לנו את הכסף. רובנו בכלל לא מודעים לזה. עם מודעות נכונה ושימוש חכם בהזדמנויות הקיימות במשק, תוכלו לחסוך אלפי שקלים בשנה.",
+
+            "cat":"דרכים לקיזוז הוצאות"
+
+        },
+
+ {
+
+            "id": 8,
+
+            "pic": "familymoney.jpg",
+
+            "title": "תקציב שבועי.  ",
+
+            "abstract": "הדרך הכי טובה, הכי יעילה והכי פשוטה להצליח להישאר מאוזנים או אפילו עם רווח גדל עם כל חודש שעובר, היא לבנות תקציב משפחתי. ",
+
+            "fullContent": "אין כמעט אף משפחה שעושה את זה בפועל לצערי, מאחר וזה קשה לבנות את התקציב המשפחתי שלכם ואחרי שבניתם אותו אתם צריכים לנהל מעקב קבוע ותדיר על כל הוצאה – של כל משק הבית.",
+
+            "cat":"טיפים"
+
+        },
+
+ {
+
+            "id": 9,
+
+            "pic": "sale.jpg",
+
+            "title": "קנייה חכמה  ",
+
+            "abstract": "לקנות במבצעים, הזמנות מהאינטרנט, השוואות מחירים. ",
+
+            "fullContent": "היום כדי שנוכל לכלכל את חיינו בחוכמה ולנהל תקציב נכון עלינו לדאוג לכך שכל קניה שאנו עושים בין אם זאת קניה בסופר, או קניה של בגדים בקניון , או קניה באינטרנט  צריכה להיות קניה חכמה שבה קנינו במחיר ריאלי וסביר מוצר  שאנו באמת זקוקים לו. קניה חכמה היא ההיפך מקניה שמקורה  בדחף רגעי או  בלחץ חברתי.",
+
+            "cat":"דרכים לקיזוז הוצאות"
+
+        },
+
+{
+
+            "id": 10,
+
+            "pic": "app.jpg",
+
+            "title": "שימוש בכלים טכנולוגיים שיעזרו בליווי.  ",
+
+            "abstract": "הטכנולוגיה יכולה לעזור לנו בצורה משמעותית.",
+
+            "fullContent": "בדיוק כמו בעסק, מעקב ותיעוד עקבי של ההוצאות והכנסות מאפשרים לנו שליטה פיננסית מלאה, מייעלים את מצבנו הכלכלי ואף מביאים לחיסכון משמעותי בהוצאות.",
+
+            "cat":"טיפים"
+
+}
+
+]
+
+}
+
+
+
+//לאחר טעינת העמוד
+document.addEventListener("DOMContentLoaded", function (event) {
+    createItems();
+});
+
+
+function createItems() {
+    // Get the items container element by its ID
+    const itemsContainer = document.getElementById('itemsContainer');
+    itemsContainer.innerHTML = "";
+    // Set the class attribute of the items container to apply styling
+    itemsContainer.setAttribute("class", "row justify-content-xs-center");
+
+     // Create a new div element to hold the list of items
+    const list = document.createElement("div");
+    list.setAttribute("class", "btn-group grid row-gap-4");
+    list.setAttribute("id", "topics-list");
+
+    // Iterate through the generators in the JSON data
+    jsonData.generators.forEach(generator => {
+        const listItem = document.createElement("div");
+        listItem.setAttribute("class", "subSubject card  col-sm-3 col-12");
+        listItem.setAttribute("id", `div_${generator.id}`);
+
+ // Create an image element for the generator item
+        const listItemImg = document.createElement("img");
+        listItemImg.setAttribute("src", `images/${generator.pic}`);
+        listItemImg.setAttribute("class", "imgCard card-img-top");
+        listItemImg.setAttribute("alt", generator.title);
+
+         // Create a div element to hold the content of the generator item
+        const contentContainer = document.createElement("div");
+        contentContainer.setAttribute("class", "content card-body");
+
+        // Create a heading element for the generator item title
+        const listItemTitle = document.createElement("h1");
+        listItemTitle.setAttribute("class", "card-title");
+        listItemTitle.innerHTML = generator.title;
+
+         // Create a paragraph element for the generator item abstract
+        const listItemAbstract = document.createElement("p");
+        listItemAbstract.setAttribute("class", "card-text");
+        listItemAbstract.innerHTML = generator.abstract;
+
+         // Create a button element for more information on the generator item
+        const listItemBtn = document.createElement("button");
+        listItemBtn.setAttribute("class", "buttonSubject btn btn-primary");
+        listItemBtn.setAttribute("id", `btn_${generator.id}`);
+        listItemBtn.innerHTML = "ראו עוד";
+
+          // Add event listeners for mouseover, mouseout, and click events on the button
+        listItemBtn.addEventListener("mouseover", hoverTopic);
+        listItemBtn.addEventListener("mouseout", outTopic);
+        listItemBtn.addEventListener("click", clickTopic);
+
+          // Append child elements to build the structure of each generator item
+        listItem.appendChild(listItemImg);
+        contentContainer.appendChild(listItemTitle);
+        contentContainer.appendChild(listItemAbstract);
+        contentContainer.appendChild(listItemBtn);
+        listItem.appendChild(contentContainer);
+        
+
+        list.appendChild(listItem);
+    });
+
+    itemsContainer.appendChild(list);
+}
+
+function hoverTopic(e) {
+    // נמצא את המזהה של הפריט עליו עברנו
+    const currentId = e.target.id.split("_")[1];
+    // נמצא את הפריט ברשימה מעלה ונשמור את הטאג שלו
+    document.getElementById(`btn_${currentId}`).classList.add("darkblue");
+}
+
+function outTopic() {
+    // נעבור על כלל האלמנטים ברשימה מעלה ונסיר מהאלמנטים שמציגים אותם את המחלקה
+    jsonData.generators.forEach(generator => {
+        // נפנה לאלמנט בדף לפי המזהה ונסיר ממנו את המחלקה
+        document.getElementById(`btn_${generator.id}`).classList.remove("darkblue");
+    });
+}
+
+function clickTopic(e) {
+    // נמצא את המזהה של הפריט עליו לחצנו
+    const currentId = e.target.id.split("_")[1];  
+    const myModal = new bootstrap.Modal(document.getElementById('myModal'));
+    myModal.show();
+    // נמצא את הפריט ברשימה מעלה ונשמור את התוכן והקישור שלו
+    const content = jsonData.generators.find(g => g.id == currentId).fullContent;
+    // נזריק את התוכן והקישור במקומות המתאימים בדף - שימו לב לתגיות בדף
+    document.querySelector(".modal-body p").innerHTML = content;
+}
+
+
+function filterItems() {
+    // פונקציה שאחראית לסנן כרסטיות לפני הקטגרויות שלהן
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    const itemsContainer = document.getElementById('itemsContainer');
+
+    jsonData.generators.forEach(generator => {
+        const listItem = document.getElementById(`div_${generator.id}`);
+        const category = generator.cat.toLowerCase();
+
+        if (searchInput === '' || category.includes(searchInput)) {
+            listItem.style.display = 'block';
+        } else {
+            listItem.style.display = 'none';
+        }
+    });
+}
+
+function toggleNav(){
+    // אחראי לסגור  את התפריט במובייל
+    document.getElementById("main-nav").classList.toggle("hide-mobile");
+}
